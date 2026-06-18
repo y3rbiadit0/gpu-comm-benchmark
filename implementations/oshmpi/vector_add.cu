1c739c1c7e37662b1f7eb78ab06ec63b1631ef73
Franco N. Merenda <merenda.franco83@gmail.com>
2026-06-18 13:22:47 +0200

    fixed oshmpi

--- implementations/oshmpi/vector_add.cu
+++ implementations/oshmpi/vector_add.cu
@@ -1,13 +1,32 @@
 #include <cuda_runtime.h>
 
+#include <cstddef>
+
 #ifndef USE_CUDA
 #define USE_CUDA 1
 #endif
 #include <shmem.h>
 #include <shmemx.h>
 
+#if defined(COMM_PLAYGROUND_OSHMPI_SHMEMX_SPACE_FALLBACK)
+typedef void* shmemx_space_t;
+typedef void* shmemx_device_handle_t;
+typedef enum { SHMEMX_MEM_HOST, SHMEMX_MEM_CUDA, SHMEMX_MEM_ZE } shmemx_memkind_t;
+typedef struct {
+  size_t sheap_size;
+  int num_contexts;
+  shmemx_memkind_t memkind;
+  shmemx_device_handle_t device_handle;
+  long hints;
+} shmemx_space_config_t;
+extern "C" void shmemx_space_create(shmemx_space_config_t, shmemx_space_t*);
+extern "C" void shmemx_space_destroy(shmemx_space_t);
+extern "C" void shmemx_space_attach(shmemx_space_t);
+extern "C" void shmemx_space_detach(shmemx_space_t);
+extern "C" void* shmemx_space_malloc(shmemx_space_t, size_t);
+#endif
+
 #include <algorithm>
-#include <cstddef>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
