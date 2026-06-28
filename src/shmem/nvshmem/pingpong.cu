1f3d6da80d3de6dab2c13aaa95c2d49c902eef10
Franco N. Merenda <merenda.franco83@gmail.com>
2026-06-28 23:55:14 +0200

    updated ping pong docs

--- src/shmem/nvshmem/pingpong.cu
+++ src/shmem/nvshmem/pingpong.cu
@@ -25,12 +25,15 @@ void check_cuda(cudaError_t status, const char* call) {
   }
 }
 
-//Device Initiated - PingPong
-// Following Best Practices for NVSHMEM APIs
-// https://docs.nvidia.com/nvshmem/release-notes-install-guide/best-practice-guide/apis.html
-// --> Sending data: https://docs.nvidia.com/nvshmem/api/gen/api/rma.html#nvshmem-put
-// --> Signaling: https://docs.nvidia.com/nvshmem/api/gen/api/signal.html#nvshmem-signal
-// --> Point-to-point synchronization: https://docs.nvidia.com/nvshmem/api/gen/api/sync.html#
+// Device Initiated - PingPong
+//  Following Best Practices for NVSHMEM APIs
+//  https://docs.nvidia.com/nvshmem/release-notes-install-guide/best-practice-guide/apis.html
+//    --> Sending data: https://docs.nvidia.com/nvshmem/api/gen/api/rma.html#nvshmem-put
+//    --> Signaling: https://docs.nvidia.com/nvshmem/api/gen/api/signal.html#nvshmem-signal
+//    --> Point-to-point synchronization: https://docs.nvidia.com/nvshmem/api/gen/api/sync.html#
+//   pe 0: send data to pe 1, signal pe 1, wait for signal from pe 1
+//   pe 1: wait for signal from pe 0, send data to pe 0, signal pe 0
+//   
 __global__ void pingpong_kernel(float* send, float* recv, std::uint64_t* flag, std::size_t count,
                                 int iterations, int pe, int peer, std::uint64_t base) {
   for (int it = 0; it < iterations; ++it) {
